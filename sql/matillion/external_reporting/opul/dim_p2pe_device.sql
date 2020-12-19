6ac9df04c2247be9bd198c2e3a4a929d3f445cc3
Vijay Tummala <vijay@Vijay.Tummala-C02ZR803MD6M>
2020-12-18 22:51:54 -0800

    Adding Dim and fact tables for opul

---
+++ sql/matillion/external_reporting/opul/dim_p2pe_device.sql
@@ -0,0 +1,12 @@
+WITH main as
+(SELECT
+ 	id AS device_id,
+ 	organization_id,
+ 	merchant_id,
+ 	label,
+ 	status,
+ 	device_uuid  
+FROM
+	p2pe_opul.p2pe_device 
+)
+SELECT * FROM main
---
+++ sql/matillion/external_reporting/opul/fact_deposit_summary.sql
@@ -0,0 +1,46 @@
+with funding as  
+(  
+SELECT
+    funding_master_id,
+    net_sales*100 AS net_sales,
+    third_party*100 AS third_party,
+    adjustment*100 AS adjustment,
+    interchange_fee*100 AS interchange_fee,
+    service_charge*100 AS service_charge,
+    fee*100 AS fee,
+    reversal*100 AS reversal,
+    other_adjustment*100 AS other_adjustment,
+    total_funding*100 AS total_funding,
+    funding_date,
+    date_added,
+    provider.encrypted_ref_id AS gx_provider_id,
+    funding.status,
+    funding.id AS funding_id  
+FROM
+    gaia_opul.funding funding 
+ JOIN
+    gaia_opul.authorisation authorisation
+        ON authorisation_id = authorisation.id  
+JOIN
+    gaia_opul.provider provider 
+        ON object_id = provider.id 
+),
+
+main as 
+( 
+SELECT
+    gx_provider_id,
+    funding_master_id,
+    interchange_fee + service_charge + fee + third_party AS fees,
+    adjustment + other_adjustment AS adjustments,
+    net_sales,
+    total_funding,
+    funding_date,
+    date_added,
+    reversal AS chargebacks,
+    status,
+    funding_id 
+FROM
+    funding 
+) 
+SELECT * FROM main
