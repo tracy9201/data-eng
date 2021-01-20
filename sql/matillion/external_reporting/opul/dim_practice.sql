47a3b7d86d0ba7bb76dc4d1da22a05d478349aa0
Ankita Karandikar <ankita.karandikar@hintmd.com>
2021-01-20 23:20:17 +0000

    dim_practice.sql edited online with Bitbucket

--- sql/matillion/external_reporting/opul/dim_practice.sql
+++ sql/matillion/external_reporting/opul/dim_practice.sql
@@ -1,22 +1,24 @@
-WITH main as
-(SELECT
-    organization_data.id AS k_practice_id,
-    gx_provider_id,
-    activated_at AS practice_activated_at,
-    timezone AS practice_time_zone,
-    lastname AS practice_name,
-    city AS practice_city,
-    state AS practice_state,
-    zip AS practice_zip  
-FROM
-    kronos_opul.organization_data  organization_data
-JOIN
-    kronos_opul.users  users        
-        ON organization_data.id = organization_id  
-JOIN
-    kronos_opul.address  address        
-        ON address.id = address_id  
-WHERE
-    role = 7
-)
-SELECT * FROM main
+
+WITH main as (SELECT
+        organization_data.id AS k_practice_id,
+        gx_provider_id,
+        activated_at AS practice_activated_at,
+        timezone AS practice_time_zone,
+        lastname AS practice_name,
+        city AS practice_city,
+        state AS practice_state,
+        zip AS practice_zip,
+        merchant_id
+    FROM
+        kronos_opul.organization_data organization_data
+    JOIN
+        kronos_opul.users users
+            ON organization_data.id = organization_id
+    JOIN
+        kronos_opul.address address
+            ON address.id = address_id
+    WHERE
+        role = 7 ) SELECT
+        *
+    FROM
+        main
