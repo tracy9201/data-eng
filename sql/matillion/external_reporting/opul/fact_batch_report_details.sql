45124cd434870b215c13f1f7d4373df5c1d14260
Vijay Tummala <vijay@Vijay.Tummala-C02ZR803MD6M>
2021-02-08 15:03:46 -0800

    Move major lookml code to run as a SQL

--- sql/matillion/external_reporting/opul/fact_batch_report_details.sql
+++ sql/matillion/external_reporting/opul/fact_batch_report_details.sql
@@ -10,11 +10,75 @@ WITH sub_cus as
 ),
 batch_report_details as
 (
-  select payment_summary.*, 
+  select 
   subscription_name, 
-  user_type
+  user_type,
+  sales_id,
+  case when sales_id like 'refund%' then 'Refund'
+      when sales_id like 'credit%'  and  sales_type in ('reward', 'credit') and sales_name = 'BD Payment' then 'Offer Redemption'
+      when sales_id like 'credit%'  and  sales_type = 'provider credit' then 'Deposit From Patient'
+      when sales_id like 'void%' then 'Void'
+      else 'Sale'
+      end as transaction,
+  case when sales_type = 'cash' and (sales_id like 'payment%' or sales_id like 'refund%') then 'Cash'
+      when sales_type = 'check' and (sales_id like 'payment%' or sales_id like 'refund%') then 'Check'
+      when sales_type = 'credit_card' and (sales_id like 'payment%' or sales_id like 'refund%') then 'Credit Card'
+      when sales_type = 'provider credit' and (sales_id like 'credit%' or sales_id like 'refund%') then 'Practice Credit'
+      when sales_type in ('reward', 'credit') and sales_name = 'BD Payment' and (sales_id like 'credit%' or sales_id like 'refund%') then 'BD'
+      when sales_type in ('credit', 'reward') and (sales_id like 'credit%' or sales_id like 'refund%') then 'Other'
+      when sales_type ='credit_card' and sales_id like 'tran%' then 'Recurring Pmt'
+      when sales_id like 'void%' or tokenization is not null then 'Credit Card'
+      end as payment_method,
+  case when sales_type = 'cash'  then 'Cash'
+      when sales_type = 'check'  then 'Check'
+      when sales_type = 'credit_card'  and substring(tokenization,2,2) like '4%' then 'Visa'
+      when sales_type = 'credit_card'  and (substring(tokenization,2,2) like '34%' or substring(tokenization,2,2) like '37%') then 'Amex'
+      when sales_type = 'credit_card'  and (substring(tokenization,2,2) like '51%' or substring(tokenization,2,2) like '52%' or substring(tokenization,2,2) like '53%' or substring(tokenization,2,2) like '54%' or substring(tokenization,2,2) like '55%') then 'Mastercard'
+      when sales_type = 'credit_card'  and (substring(tokenization,2,2) like '60%' or substring(tokenization,2,2) like '65%' ) then 'Discover'
+      when sales_type = 'credit_card'  and (substring(tokenization,2,2) not like '4%' or substring(tokenization,2,2) not like '34%' or substring(tokenization,2,2) not like '37%' or substring(tokenization,2,2) not like '51%' or substring(tokenization,2,2) not like '52%' or substring(tokenization,2,2) not like '53%' or substring(tokenization,2,2) not like '54%' or substring(tokenization,2,2) not like '55%' or substring(tokenization,2,2) not like '60%' or substring(tokenization,2,2) not like '65%') then 'Other Credit Card'
+      when sales_type = 'reward' then 
+      when sales_type = 'provider credit' then 'Practice Credit'
+      when sales_type = 'adjustment' then 'Adjustment'
+      when sales_type = 'credit' then 'Coupon'
+      when sales_type in ('reward', 'credit') and sales_name = 'BD Payment' and (sales_id like 'credit%' or sales_id like 'refund%') then 'BD'
+      end 
+  as payment_detail,
+  case 
+      when user_type=1 then subscription_name
+      when sales_type = 'check' then null
+      when sales_type = 'credit_card' and (sales_id like 'payment%' or sales_id like 'refund%') then null
+      when sales_type in ('reward', 'credit') and sales_name = 'BD Payment' and (sales_id like 'credit%' or sales_id like 'refund%') then null
+      when sales_type ='credit_card' and sales_id like 'tran%' then null
+      when sales_type = 'Recurring Pmt' and sales_type = 'credit_card' then null
+      else payment_id end
+  as description,
+  case 
+       when sales_type = 'check' then payment_id
+       when sales_type ='credit_card' and sales_id like 'tran%' then CONCAT('**** ',cast(payment_id as VARCHAR))
+       when sales_type = 'credit_card' and (sales_id like 'payment%' or sales_id like 'refund%') then CONCAT('**** ',cast(payment_id as VARCHAR))
+       else null end 
+  as payment_id,
+  gx_customer_id,
+  gx_provider_id,
+  case when (sales_id like 'void1%' or sales_id like 'void2%')  then sales_created_at + INTERVAL '1 DAY' 
+      else sales_created_at END  as sales_created_at,
+  sales_created_at as original_sales_created_at,
+  staff_user_id,
+  device_id,
+  case 
+      when sales_id like 'refund%' then coalesce((-1*sales_amount)/100,0)
+      when sales_id like 'void%' then coalesce((-1*sales_amount)/100,0)
+      else coalesce(sales_amount/100,0) end
+  as sales_amount,
+  coalesce((gratuity_amount)/100,0) as gratuity_amount
   from dwh_opul.fact_payment_summary payment_summary
   left join sub_cus as sc on payment_summary.gx_customer_id = sc.gx_cus_id
   where sc.sub_created = 1
+),
+main as
+(
+  select *,
+  extract (epoch from sales_created_at) as epoch_sales_created_at,
+  extract (epoch from original_sales_created_at) as epoch_original_sales_created_at,
 )
-select * from batch_report_details
+select * from main
