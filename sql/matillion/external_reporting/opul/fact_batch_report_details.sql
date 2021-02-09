dc14e2bae9f370217b5d9060b6196bf8e41d0612
Vijay Tummala <vijay@Vijay.Tummala-C02ZR803MD6M>
2021-02-08 22:34:03 -0800

    change fact_payment_summary_1 to fact_payment_summary

--- sql/matillion/external_reporting/opul/fact_batch_report_details.sql
+++ sql/matillion/external_reporting/opul/fact_batch_report_details.sql
@@ -36,7 +36,8 @@ batch_report_details as
       when sales_type = 'credit_card'  and (token_substr like '34%' or token_substr like '37%') then 'Amex'
       when sales_type = 'credit_card'  and (token_substr like '51%' or token_substr like '52%' or token_substr like '53%' or token_substr like '54%' or token_substr like '55%') then 'Mastercard'
       when sales_type = 'credit_card'  and (token_substr like '60%' or token_substr like '65%' ) then 'Discover'
-      when sales_type = 'credit_card'  and (token_substr not like '4%' or token_substr not like '34%' or token_substr not like '37%' or token_substr not like '51%' or token_substr not like '52%' or token_substr not like '53%' or token_substr not like '54%' or token_substr not like '55%' or token_substr not like '60%' or token_substr not like '65%') then 'Other Credit Card'
+      --when sales_type = 'credit_card'  and (token_substr not like '4%' or token_substr not like '34%' or token_substr not like '37%' or token_substr not like '51%' or token_substr not like '52%' or token_substr not like '53%' or token_substr not like '54%' or token_substr not like '55%' or token_substr not like '60%' or token_substr not like '65%') then 'Other Credit Card'
+      when sales_type = 'credit_card' then 'Other Credit Card'
       when sales_type = 'reward' then 'Reward'
       when sales_type = 'provider credit' then 'Practice Credit'
       when sales_type = 'adjustment' then 'Adjustment'
@@ -71,7 +72,7 @@ batch_report_details as
       else coalesce(sales_amount/100,0) end
   as sales_amount,
   coalesce((gratuity_amount)/100,0) as gratuity_amount
-  from dwh_opul.fact_payment_summary_1 payment_summary
+  from dwh_opul.fact_payment_summary payment_summary
   left join sub_cus as sc on payment_summary.gx_customer_id = sc.gx_cus_id
   where sc.sub_created = 1
 ),
