9b2ba3b8801ec05570f5253bbb61759b078f7879
Ankita Karandikar <ankita.karandikar@hintmd.com>
2021-03-05 23:51:08 +0000

    ddl_dim_customer.sql created online with Bitbucket

---
+++ sql/matillion/external_reporting/hintmd/ddl/ddl_dim_customer.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS dwh_hint.dim_customer;
+
+CREATE TABLE IF NOT EXISTS dwh_hint.dim_customer
+(
+  k_customer_id BIGINT   ENCODE raw
+  ,member_on_boarding_date TIMESTAMP WITHOUT TIME ZONE   ENCODE raw
+  ,member_cancel_date TIMESTAMP WITHOUT TIME ZONE   ENCODE raw
+  ,customer_email VARCHAR(255)   ENCODE raw
+  ,customer_mobile VARCHAR(MAX)   ENCODE raw
+  ,customer_gender SMALLINT   ENCODE raw
+  ,customer_birth_year NUMERIC(18,0)   ENCODE raw
+  ,gx_customer_id VARCHAR(max)   ENCODE raw
+  ,member_type VARCHAR(16)   ENCODE raw
+  ,customer_city VARCHAR(64)   ENCODE raw
+  ,customer_state VARCHAR(MAX)   ENCODE raw
+  ,customer_zip VARCHAR(16)   ENCODE raw
+  ,user_type INTEGER  ENCODE raw
+  ,customer_type VARCHAR(MAX) ENCODE raw
+  ,firstname VARCHAR(MAX)   ENCODE raw
+  ,lastname  VARCHAR(MAX)  ENCODE raw
+    ,primary key(k_customer_id)
+    ,UNIQUE(gx_customer_id)
+)
+DISTKEY (gx_customer_id)
+SORTKEY (gx_customer_id,user_type)
+;
