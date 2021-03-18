381ff5e410fa0dcb1c185e7abf102f25cb53b3a3
Larry Hui <larry@hintmd.com>
2021-03-18 04:36:37 +0000

    ddl_staff.sql

---
+++ sql/matillion/ir_commercial/DDL/ddl_staff.sql
@@ -0,0 +1,20 @@
+
+
+CREATE TABLE IF NOT EXISTS ir_commercial.staff (   
+    user_id BIGINT   ENCODE raw   ,
+    title VARCHAR(255)   ENCODE raw   ,
+    firstname VARCHAR(MAX)   ENCODE raw   ,
+    lastname VARCHAR(MAX)   ENCODE raw   ,
+    role VARCHAR(255)   ENCODE raw   ,
+    email VARCHAR(MAX)   ENCODE raw   ,
+    organization_id BIGINT   ENCODE raw   ,
+    created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE raw   ,
+    deprecated_at TIMESTAMP WITHOUT TIME ZONE   ENCODE raw   ,
+    zendesk_user_id BIGINT   ENCODE raw   ,
+    pricinple_epxert boolean   ENCODE raw   ,
+    commssion_percentage NUMERIC(18,2)   ENCODE raw   ,
+    primary key(user_id) 
+)
+DISTSTYLE ALL
+SORTKEY (user_id)
+;
