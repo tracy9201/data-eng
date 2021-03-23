dd79bd18a58a3df80badf848328894afce5a6812
Larry Hui <larry@hintmd.com>
2021-03-23 22:19:46 +0000

    dim_brand_catalog_offering.sql created online with Bitbucket

---
+++ sql/matillion/external_reporting/opul/dim_brand_catalog_offering.sql
@@ -0,0 +1,33 @@
+WITH main as (
+select  
+    offering.id as offering_id, 
+    offering.name as offering_name, 
+    catalog_item.name as catalog_name, 
+    catalog_item_id as catalog_id, 
+    brand.name as factory_name, 
+    catalog_item.brand_id as factory_id, 
+    round(cast(offering.pay_in_full_price as numeric )/100,2) as offering_pay_in_full_price, 
+    round(cast(offering.subscription_price as numeric )/100,2) as offering_subscription_price, 
+    round(cast(offering.pay_over_time_price as numeric )/100,2) as offering_pay_over_time_price, 
+    round(cast(offering.service_tax as numeric )/100,2) as offering_service_tax, 
+    offering.status as offering_status, 
+    offering.organization_id, 
+    offering.created_at as offering_created_at, 
+    catalog_item.created_at as catalog_created_at, 
+    catalog_item.status as catalog_status, 
+    round(cast(catalog_item.wholesale_price as numeric )/100,2) as catalog_wholesale_price, 
+    catalog_item.bd_status, 
+    catalog_item.sku,
+    brand.created_at as brand_created_at,
+    offering.created_at,
+    offering.updated_at,
+    current_timestamp::timestamp as dwh_created_at
+from  kronos_opul.offering offering
+left join 
+    kronos_opul.catalog_item catalog_item
+        on offering.catalog_item_id = catalog_item.id
+left join  
+    kronos_opul.brand brand
+        on catalog_item.brand_id = brand.id
+)
+SELECT * FROM main
