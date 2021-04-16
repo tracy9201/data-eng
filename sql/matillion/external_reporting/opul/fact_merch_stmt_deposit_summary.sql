91ed5eeceaebd78594104a3bfef8bd08cfaba229
Vijay Tummala <vijay.tummala@revance.com>
2021-04-16 13:34:18 -0700

    adding range_with_year and editing card_brand logic

--- sql/matillion/external_reporting/opul/fact_merch_stmt_deposit_summary.sql
+++ sql/matillion/external_reporting/opul/fact_merch_stmt_deposit_summary.sql
@@ -3,6 +3,7 @@ WITH month_date_range as
 SELECT
     funding_month
     ,to_char(min( date_trunc('month', funding_date)::date),'MM/DD') || '-' ||to_char(max(last_day(funding_date)),'MM/DD') as range
+    ,to_char(min( date_trunc('month', funding_date)::date),'MM/DD/YYYY') || '-' ||to_char(max(last_day(funding_date)),'MM/DD/YYYY') as range_with_year
 FROM  
 	dwh_opul.fact_merch_stmt_deposit_details
 GROUP BY  1
@@ -15,6 +16,7 @@ SELECT
 	b.merchant_id
 	,a.funding_month
     ,a.range
+    ,a.range_with_year
 	,sum(coalesce(b.transactions,0)) as Transactions
 	,sum(coalesce(b.charges,0)) as Charges
 	,sum(coalesce(b.refunds,0)) as Refunds
@@ -28,7 +30,7 @@ FROM
 	dwh_opul.fact_merch_stmt_deposit_details b
 JOIN 
 	month_date_range a on a.funding_month = b.funding_month
-GROUP BY 1,2,3,10,11
+GROUP BY 1,2,3,4,12,13
 )
 
 SELECT * FROM main
