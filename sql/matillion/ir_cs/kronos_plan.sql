39d250c9d47430c68cc329160e04499ec451df1b
Larry Hui <larry.hui@revance.com>
2021-05-03 23:36:05 +0000

    kronos_plan.sql created online with Bitbucket

---
+++ sql/matillion/ir_cs/kronos_plan.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS ir_cs.kronos_plan;
+
+CREATE TABLE IF NOT EXISTS ir_cs.kronos_plan (
+    plan_id int4,
+    gx_plan_id varchar(64),
+    user_id int4,
+    plan_first_date date,
+    created_at timestamp,
+    updated_at timestamp,
+    deprecated_at timestamp,
+    plan_active int4,
+    plan_ended_date date,
+    member_recoganize int4,
+    num_of_subscription int4,
+    on_boarding_date timestamp
+
+      ,primary key(plan_id)
+      ,UNIQUE(plan_id)
+)
+DISTKEY (plan_id)
+SORTKEY (plan_id)
+
+;
