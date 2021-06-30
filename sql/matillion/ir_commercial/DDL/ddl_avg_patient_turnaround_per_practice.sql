9c6a24e2777c351e17a6da727dd03499f4d073e2
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-06-30 22:54:30 +0000

    Added category and changes in the name abbrevation

--- sql/matillion/ir_commercial/DDL/ddl_avg_patient_turnaround_per_practice.sql
+++ sql/matillion/ir_commercial/DDL/ddl_avg_patient_turnaround_per_practice.sql
@@ -3,8 +3,9 @@ DROP TABLE IF EXISTS ir_commercial.avg_patient_turnaround_per_practice;
 CREATE TABLE IF NOT EXISTS ir_commercial.avg_patient_turnaround_per_practice
 (
   business_name VARCHAR(256)    ENCODE raw,
-  practice_id BIGINT ENCODE raw,
-  avg_days int   ENCODE raw,
-  primary key(practice_id)
+  gx_provider_id BIGINT ENCODE raw,
+  category VARCHAR(256)    ENCODE raw,
+  AverageDays int   ENCODE raw,
+  primary key(gx_provider_id)
 )
 ;
