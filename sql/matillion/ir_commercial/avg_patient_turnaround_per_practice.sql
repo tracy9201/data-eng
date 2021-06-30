57162386366a10782a71da8ced5dc439cf8d14ef
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-06-30 22:51:26 +0000

    Code change for frequency type for business, including Category

--- sql/matillion/ir_commercial/avg_patient_turnaround_per_practice.sql
+++ sql/matillion/ir_commercial/avg_patient_turnaround_per_practice.sql
@@ -1,56 +1,91 @@
-with DurationPerUser as 
+with Chain as
 (
-   SELECT DISTINCT
-      user_id,
-      Date(service_date) AS Service_Date,
-      COALESCE(Date(LAG(service_date, 1) OVER (PARTITION BY user_id, offering_id 
-   ORDER BY
-      service_date)), Date(service_date)) AS Last_Service_Date,
-      COALESCE(DATEDIFF(day, Last_Service_Date, service_date), 0) AS Diff_Days 
-   FROM
-      ir_commercial.fulfillment 
-   WHERE
-      status = 'Completed' 
-   ORDER BY
-      user_id,
-      service_date DESC 
+   with DurationPerUser as
+   (
+      SELECT DISTINCT
+         gx_customer_id,
+         Date(service_date) AS Service_Date,
+         COALESCE(Date(LAG(service_date, 1) OVER (PARTITION BY gx_customer_id, offering_id
+      ORDER BY
+         service_date)), Date(service_date)) AS Last_Service_Date,
+         COALESCE(DATEDIFF(day, Last_Service_Date, service_date), 0) AS Diff_Days
+      FROM
+         ir_commercial.fulfillment
+      WHERE
+         status = 'Completed'
+      ORDER BY
+         gx_customer_id,
+         service_date DESC
+   )
+,
+   Brand as
+   (
+      SELECT
+         ICF.gx_customer_id,
+         IR.category
+      FROM
+         ir_cs.brand_catalog_category IR
+         Inner JOIN
+            ir_commercial.offering IO
+            ON (IO.factory_name = IR.brand)
+            AND
+            (
+               IO.catalog_name = IR.catalog
+            )
+         INNER JOIN
+            ir_commercial.fulfillment ICF
+            ON (ICF.offering_id = IO.offering_id)
+   )
+   Select
+      DurationPerUser.gx_customer_id,
+      DurationPerUser.Service_Date,
+      DurationPerUser.Diff_Days,
+      Brand.category
+   from
+      DurationPerUser
+      inner join
+         Brand
+         on Brand.gx_customer_id = DurationPerUser.gx_customer_id
 )
 ,
-AveragePerPractice AS 
+AveragePerPractice AS
 (
    select
-      practice.practice_id,
-      avg(DurationPerUser.diff_days) as AverageDays 
+      practice.gx_provider_id,
+      Chain.category,
+      avg(Chain.diff_days) as AverageDays
    from
-      DurationPerUser 
+      Chain
       inner join
-         ir_commercial.patient 
-         on patient.user_id = DurationPerUser.user_id 
+         ir_commercial.patient
+         on patient.gx_customer_id = Chain.gx_customer_id
       inner join
-         ir_commercial.practice 
-         on practice.practice_id = patient.organization_id 
-         and diff_days > 0 
+         ir_commercial.practice
+         on practice.gx_provider_id = patient.gx_provider_id
+         and diff_days > 0
    group by
-      practice_id 
+      practice.gx_provider_id,
+      category
    order by
-      practice_id 
+      practice.gx_provider_id
 )
 ,
 Result as
 (
    select
-      practice.business_name,
-      practice.practice_id,
-      AveragePerPractice.AverageDays 
+      ir_commercial.practice.business_name,
+      ir_commercial.practice.gx_provider_id,
+      AveragePerPractice.category,
+      AveragePerPractice.AverageDays
    from
-      AveragePerPractice 
+      AveragePerPractice
       inner join
-         ir_commercial.practice 
-         on practice.practice_id = AveragePerPractice.practice_id 
+         ir_commercial.practice
+         on ir_commercial.practice.gx_provider_id = AveragePerPractice.gx_provider_id
    order by
-      practice.business_name 
+      practice.business_name
 )
 Select
-   * 
+   *
 from
    Result
