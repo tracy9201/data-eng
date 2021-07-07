1eb0b6c624e944a980ef1fc75d02c28011c419db
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-07-07 21:34:10 +0000

    Adding DDL for monthly retention

---
+++ sql/matillion/ir_commercial/DDL/monthly_retention.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS ir_commercial.monthly_retention;
+
+CREATE TABLE IF NOT EXISTS ir_commercial.monthly_retention
+(
+  created DATE ENCODE raw,
+  gx_customer_id BIGINT ENCODE raw,
+  cancelled_month int   ENCODE raw,
+  created_month TIMESTAMP WITHOUT TIME ZONE   ENCODE raw
+  primary key(gx_customer_id)
+)
+;
