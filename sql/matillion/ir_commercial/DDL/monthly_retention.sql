5d35f132ee938990de2e524d0e099e1b68c5fe0a
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-07-08 23:37:56 +0000

    Columns added in the monthly_retention

--- sql/matillion/ir_commercial/DDL/monthly_retention.sql
+++ sql/matillion/ir_commercial/DDL/monthly_retention.sql
@@ -5,7 +5,10 @@ CREATE TABLE IF NOT EXISTS ir_commercial.monthly_retention
   created DATE ENCODE raw,
   gx_customer_id BIGINT ENCODE raw,
   cancelled_month int   ENCODE raw,
-  created_month TIMESTAMP WITHOUT TIME ZONE   ENCODE raw
+  created_month TIMESTAMP WITHOUT TIME ZONE   ENCODE raw,
+  total BIGINT ENCODE raw,
+  sum BIGINT ENCODE raw,
+  agg_total BIGINT ENCODE raw,
   primary key(gx_customer_id)
 )
 ;
