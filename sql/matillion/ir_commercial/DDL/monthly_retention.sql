76f9e5f53264912d91dfb4b09b1f409604325c12
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-07-13 21:15:01 +0000

    DDl column changes for monthly retention

--- sql/matillion/ir_commercial/DDL/monthly_retention.sql
+++ sql/matillion/ir_commercial/DDL/monthly_retention.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS ir_commercial.monthly_retention
   gx_customer_id BIGINT ENCODE raw,
   cancelled_month int   ENCODE raw,
   created_month TIMESTAMP WITHOUT TIME ZONE   ENCODE raw,
-  total BIGINT ENCODE raw,
-  TotalCustomerPerDay BIGINT ENCODE raw,
-  agg_total BIGINT ENCODE raw,
+  currently_remaning_customer BIGINT ENCODE raw,
+  Total_Customer_Per_Month BIGINT ENCODE raw,
+  customer_index BIGINT ENCODE raw,
   primary key(gx_customer_id)
 )
 ;
