787d853d96e78fc4b9188fd4b91f375dea10bab2
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-07-21 19:24:00 +0000

    Removed column

--- sql/matillion/ir_commercial/DDL/ddl_monthly_retention.sql
+++ sql/matillion/ir_commercial/DDL/ddl_monthly_retention.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS ir_commercial.monthly_retention;
 
 CREATE TABLE IF NOT EXISTS ir_commercial.monthly_retention
 (
-  created DATE ENCODE raw,
   gx_customer_id varchar(64) ENCODE raw,
   cancelled_month int   ENCODE raw,
   created_month TIMESTAMP WITHOUT TIME ZONE   ENCODE raw,
