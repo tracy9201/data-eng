cccc64f564440ca655bc94f2160a8fa1fe768068
Larry Hui <larry.hui@revance.com>
2021-07-26 18:34:42 +0000

    fact_deposit_summary.sql edited online with Bitbucket add device_fee

--- sql/matillion/external_reporting/opul/fact_deposit_summary.sql
+++ sql/matillion/external_reporting/opul/fact_deposit_summary.sql
@@ -15,7 +15,30 @@ FROM
 WHERE 
     status = 'SETTLED' 
 ),
+instruction_settled_date AS
+(
+   SELECT 
+     funding_instruction_id,
+     settled_at AS settled_at_date
+   FROM odf.fiserv_transaction
+   GROUP BY 1,2
+),
 
+device_fee AS
+(
+  SELECT 
+    fee.funding_instruction_id  as reference_id
+    ,isd.settled_at_date as funding_date
+    , fee.mid AS merchant_id
+    , 0 as adjustments
+    , fee.amount /100.0 AS fees
+    , 0 AS net_sales
+    , 0 as chargebacks
+  FROM odf.non_transactional_fee fee
+  INNER JOIN
+      instruction_settled_date isd ON isd.funding_instruction_id = fee.funding_instruction_id
+  WHERE fee.settlement_id is not null
+),
 payfac as
 (
 SELECT 
@@ -28,13 +51,18 @@ SELECT
     chargebacks/100.0 as chargebacks
 FROM funding_instruction
 ),
+main_union AS(
+    SELECT * from payfac
+    UNION
+    SELECT * from device_fee
+),
 
 main AS
 (
 SELECT *, 
     extract(epoch from funding_date) as epoch_funding_date,
     current_timestamp::timestamp as dwh_created_at
-FROM payfac
+FROM main_union
 )
 
 SELECT * FROM main
