039e232129abe2ed91590284861e5383180547eb
Vijay Tummala <vijay.tummala@revance.com>
2021-07-26 11:14:17 -0700

    delete a list of user Ids

---
+++ Platform/looker/delete_embed_users.py
@@ -0,0 +1,30 @@
+import lookerapi as looker
+from lookerapi.rest import ApiException
+from pprint import pprint
+
+base_url = os.environ.get("prod_looker_base_url")
+client_id = os.environ.get("prod_looker_client_id")
+client_secret = os.environ.get("prod_looker_client_secret")
+
+unauthenticated_client = looker.ApiClient(base_url)
+unauthenticated_authApi = looker.ApiAuthApi(unauthenticated_client)
+
+token = unauthenticated_authApi.login(client_id=client_id, client_secret=client_secret)
+client = looker.ApiClient(base_url, 'Authorization', 'token ' + token.access_token)
+
+userApi = looker.UserApi(client)
+me = userApi.me();
+print(me)
+
+
+try: 
+    # Delete User
+    f = open("stage_users_to_disable_0608.txt", "r")
+    for x in f:
+        print(x.rstrip())
+        user_id = x.rstrip()
+        api_response = userApi.delete_user(user_id)
+        pprint(api_response)
+except ApiException as e:
+    print("Exception when calling UserApi->delete_user: %s\n" % e)
+
