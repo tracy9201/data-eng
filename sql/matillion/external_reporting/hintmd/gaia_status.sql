67de740f70ca5df34d61959c51310ff894ec4c54
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-07-28 21:58:31 +0000

    Changed the structure and added in description values

--- sql/matillion/external_reporting/hintmd/gaia_status.sql
+++ sql/matillion/external_reporting/hintmd/gaia_status.sql
@@ -1,43 +1,23 @@
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-1, 'UPDATED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-2, 'TO_BE_CANCELLED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-3, 'CANCELLED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-4, 'DELETED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-5, 'EDITED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-6, 'EXPIRED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-7, 'UNKNOWN');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (0, 'INACTIVE');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (1, 'ACTIVE');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (2, 'PENDING');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (3, 'TO_BE_BILLED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (4, 'PROCESSED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (7, 'DELIVERED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (10, 'CONFIRMED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (12, 'APPROVED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (15, 'PASTDUE');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (16, 'REJECTED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (18, 'PARTIALLY_COMPLETED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (20, 'COMPLETED');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (25, 'FORGIVEN');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (30, 'COLLECTION');
-
-INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (99, 'FAILED');
+INSERT INTO dwh_hint.ddl_gaia_status VALUES
+(-1, 'UPDATED',''),
+(-2, 'TO_BE_CANCELLED',''),
+(-3, 'CANCELLED',''),
+(-4, 'DELETED',''),
+(-5, 'EDITED',''),
+(-6, 'EXPIRED',''),
+(-7, 'UNKNOWN',''),
+(0, 'INACTIVE',''),
+(1, 'ACTIVE',''),
+(2, 'PENDING',''),
+(3, 'TO_BE_BILLED',''),
+(4, 'PROCESSED',''),
+(7, 'DELIVERED',''),
+(10, 'CONFIRMED','Transaction confirmed but not approved'),
+(12, 'APPROVED','Transaction approved'),
+(15, 'PASTDUE','Payment is not yet done'),
+(16, 'REJECTED',''),
+(18, 'PARTIALLY_COMPLETED',''),
+(20, 'COMPLETED',''),
+(25, 'FORGIVEN',''),
+(30, 'COLLECTION',''),
+(99, 'FAILED','');
