a258e56369316f57dfbce512c9c21af4ae7bd7be
Larry Hui <larry.hui@revance.com>
2021-07-28 16:42:13 +0000

    ddl_fact_merch_stmt_deposit_transaction_details.sql add device fee

--- sql/matillion/external_reporting/opul/parameterized/ddl/ddl_fact_merch_stmt_deposit_transaction_details.sql
+++ sql/matillion/external_reporting/opul/parameterized/ddl/ddl_fact_merch_stmt_deposit_transaction_details.sql
@@ -12,6 +12,7 @@ merchant_id VARCHAR(256)   ENCODE raw
 ,chargebacks NUMERIC(18,2)   ENCODE raw
 ,adjustments NUMERIC(18,2)   ENCODE raw
 ,fees NUMERIC(18,6)   ENCODE raw
+,non_transaction_fee NUMERIC(18,6)   ENCODE raw
 ,cp_or_cnp  VARCHAR(20)   ENCODE raw
 ,card_brand  VARCHAR(256)   ENCODE raw
 ,percent_fee NUMERIC(18,2)   ENCODE raw
