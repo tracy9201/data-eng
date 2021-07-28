28dec57468386640782b16f3a26994c38a7cf575
Laura Herron <laura.herron@revance.com>
2021-07-28 23:25:13 +0000

    ddl_kronos_status_hint.sql edited online with Bitbucket
    Updated the names of the columns and added a column for description

--- sql/matillion/external_reporting/hintmd/ddl/ddl_kronos_status_hint.sql
+++ sql/matillion/external_reporting/hintmd/ddl/ddl_kronos_status_hint.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS dwh_hint.kronos_status_hint;
 
 CREATE TABLE IF NOT EXISTS dwh_hint.kronos_status_hint (
-status_key INTEGER ENCODE raw
-, status_definition VARCHAR(255) ENCODE raw
+id INTEGER ENCODE raw
+, status VARCHAR(255) ENCODE raw
+, description VARCHAR(255) ENCODE raw
 );
