261197ad72f9c16de6f128d09414b5bc558ccbeb
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-07-28 20:32:24 +0000

    gaia_status.sql created online with Bitbucket

---
+++ sql/matillion/external_reporting/hintmd/gaia_status.sql
@@ -0,0 +1,43 @@
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-1, 'UPDATED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-2, 'TO_BE_CANCELLED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-3, 'CANCELLED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-4, 'DELETED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-5, 'EDITED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-6, 'EXPIRED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (-7, 'UNKNOWN');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (0, 'INACTIVE');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (1, 'ACTIVE');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (2, 'PENDING');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (3, 'TO_BE_BILLED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (4, 'PROCESSED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (7, 'DELIVERED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (10, 'CONFIRMED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (12, 'APPROVED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (15, 'PASTDUE');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (16, 'REJECTED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (18, 'PARTIALLY_COMPLETED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (20, 'COMPLETED');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (25, 'FORGIVEN');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (30, 'COLLECTION');
+
+INSERT INTO dwh_hint.ddl_gaia_status (status_key, status_defintion) VALUES (99, 'FAILED');
