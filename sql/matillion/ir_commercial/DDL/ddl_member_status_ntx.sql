c0c1613cb4da0188f650cb755064ca62e5a44050
Laura Herron <laura.herron@revance.com>
2021-07-29 21:14:20 +0000

    ddl_member_status_ntx.sql created online with Bitbucket
    ddl for Member status ntx , same as member_status except added category and catalog as dimensions as well.

---
+++ sql/matillion/ir_commercial/DDL/ddl_member_status_ntx.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS ir_commercial.member_status_ntx;
+
+CREATE TABLE IF NOT EXISTS ir_commercial.member_status_ntx
+(
+  id bigint ENCODE raw
+  ,gx_customer_id VARCHAR(64)   ENCODE raw
+  ,catalog VARCHAR(255) ENCODE raw
+  ,category VARCHAR(255) ENCODE raw
+  ,created_date DATE   ENCODE raw
+  ,updated_date DATE  ENCODE raw
+  ,status VARCHAR(64)   ENCODE raw
+  ,gx_provider_id VARCHAR(64)   ENCODE raw
+  ,primary key(id)
+)
+;
