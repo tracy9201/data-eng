a31beee64ec5fc6fa9eb5772a41e893ba7a28958
Joe Shea <joe.shea@revance.com>
2021-08-04 14:39:07 -0400

    renaming for consistency

--- redshift/admin/data_governance/pyaes_enc_customer_first_and_last_name.sql
+++
@@ -1,9 +0,0 @@
-UPDATE dwh_opul.dim_customer
-SET lastname = udf_enc.aes_encrypt(lastname, LPAD('customerlastnameKey/fhci5=dnv84', 16, 'z'))
-;
-commit;
-
-UPDATE dwh_opul.dim_customer
-SET firstname = udf_enc.aes_encrypt(firstname, LPAD('customerfirstnameKey/fhci5=dnv84', 16, 'z'))
-;
-commit;
