5e8b65a436a413884fa72629c92128b5d526736e
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-08-09 22:02:30 +0000

    made changes to the primary key

--- sql/matillion/external_reporting/hintmd/ddl/ddl_kronos_status_hint.sql
+++ sql/matillion/external_reporting/hintmd/ddl/ddl_kronos_status_hint.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS dwh_hint${environment}.kronos_status_hint;
 
-CREATE TABLE IF NOT EXISTS dwh_hint${environment}.kornos_status_hint 
+CREATE TABLE IF NOT EXISTS dwh_hint${environment}.kronos_status_hint 
 (
     ID BIGINT ENCODE raw,
     status VARCHAR(255) ENCODE raw,
     Description VARCHAR(255) ENCODE raw,
-    UNIQUE(ID)
+    primary key(ID)
 )
 diststyle all;
