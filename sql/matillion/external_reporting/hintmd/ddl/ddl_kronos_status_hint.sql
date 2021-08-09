6ba6d0b5c3749692ad4c1bf74a09ecf7d3d6060f
Vignesh Thulasi Dass <vignesh.dass@revance.com>
2021-08-09 21:40:20 +0000

    ddl_kronos_status_hint.sql edited online with Bitbucket

--- sql/matillion/external_reporting/hintmd/ddl/ddl_kronos_status_hint.sql
+++ sql/matillion/external_reporting/hintmd/ddl/ddl_kronos_status_hint.sql
@@ -1,4 +1,10 @@
-CREATE TABLE (
-status_key INTEGER ENCODE raw
-, status_definition VARCHAR(255) ENCODE raw
-);
+DROP TABLE IF EXISTS dwh_hint${environment}.kronos_status_hint;
+
+CREATE TABLE IF NOT EXISTS dwh_hint${environment}.kornos_status_hint 
+(
+    ID BIGINT ENCODE raw,
+    status VARCHAR(255) ENCODE raw,
+    Description VARCHAR(255) ENCODE raw,
+    UNIQUE(ID)
+)
+diststyle all;
