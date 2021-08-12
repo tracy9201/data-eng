55ea0a64d68865ff678e00766869bef38416a621
Vignesh Thulasi Dass <58238424+vigneshthulasidass@users.noreply.github.com>
2021-08-12 15:39:36 -0400

    removed spaces

--- sql/matillion/external_reporting/hintmd/ddl/ddl_gaia_status_hint.sql
+++ sql/matillion/external_reporting/hintmd/ddl/ddl_gaia_status_hint.sql
@@ -1,18 +1,10 @@
 DROP TABLE IF EXISTS ir_metadata.gaia_status_hint;
 
-
-CREATE TABLE IF NOT EXISTS ir_metadata.gaia_status_hint 
-
+CREATE TABLE IF NOT EXISTS ir_metadata.gaia_status_hint
 (
-
     id BIGINT ENCODE raw,
-
     status VARCHAR(255) ENCODE raw,
-
     description VARCHAR(255) ENCODE raw,
-
     primary key(id)
-
 )
-
 diststyle all;
