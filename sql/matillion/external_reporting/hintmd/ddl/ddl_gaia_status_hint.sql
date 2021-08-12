4eeca9e4d64779df8dafdda9a8c24c861368da58
Vignesh Thulasi Dass <58238424+vigneshthulasidass@users.noreply.github.com>
2021-08-12 15:23:47 -0400

    adding new schema name to gaia and removing _ in the status
    Adding ne

--- sql/matillion/external_reporting/hintmd/ddl/ddl_gaia_status_hint.sql
+++ sql/matillion/external_reporting/hintmd/ddl/ddl_gaia_status_hint.sql
@@ -1,10 +1,18 @@
-DROP TABLE IF EXISTS ir_metadata.kronos_status_hint;
+DROP TABLE IF EXISTS ir_metadata.gaia_status_hint;
 
-CREATE TABLE IF NOT EXISTS ir_metadata.kronos_status_hint 
+
+CREATE TABLE IF NOT EXISTS ir_metadata.gaia_status_hint 
+
 (
+
     id BIGINT ENCODE raw,
+
     status VARCHAR(255) ENCODE raw,
+
     description VARCHAR(255) ENCODE raw,
+
     primary key(id)
+
 )
+
 diststyle all;
