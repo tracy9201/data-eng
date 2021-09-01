c20e39e4e0da69610e2536188627826c2a3a0505
Nikhila Kulkarni <nikhila.kulkarni@revance.com>
2021-09-01 16:40:23 -0700

    Removed Device Fee CTE from batch and deposit summary facts

--- sql/matillion/external_reporting/opul/parameterized/sql/fact_batch_deposit_summary.sql
+++ sql/matillion/external_reporting/opul/parameterized/sql/fact_batch_deposit_summary.sql
@@ -16,22 +16,6 @@ instruction_settled_date AS
    GROUP BY 1,2
 ),
 
-device_fee AS
-(
-  SELECT 
-    fee.funding_instruction_id  as reference_id
-    ,isd.settled_at_date as batch_date
-    , isd.settled_at_date
-    , fee.chained_mid AS merchant_id
-    , 0.0 as adjustments
-    , CAST(fee.amount /100.0 as decimal(10,2)) AS fees
-    , 0.0 AS net_sales
-    , 0.0 as chargebacks
-  FROM odf${environment}.non_transactional_fee fee
-  INNER JOIN
-      instruction_settled_date isd ON isd.funding_instruction_id = fee.funding_instruction_id
-  WHERE fee.transaction_type = 'DEVICE_ORDER'
-),
 
 funding_instruction as
 (
@@ -68,11 +52,6 @@ SELECT
 FROM funding_instruction
 ),
 
-main_union AS(
-    SELECT * from payfac
-    UNION
-    SELECT * from device_fee
-),
 
 main AS
 (
@@ -80,7 +59,7 @@ SELECT *,
     extract (epoch from CONVERT_TIMEZONE('America/Los_Angeles','UTC',batch_date)) as epoch_batch_date,
     extract (epoch from CONVERT_TIMEZONE('America/Los_Angeles','UTC',settled_at_date))  as epoch_settled_at_date,
     current_timestamp::timestamp as dwh_created_at
-FROM main_union
+FROM payfac
 )
 
 SELECT * FROM main
