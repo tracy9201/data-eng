48255dd9d8117c157d30fa22b5a159f1dc9223b2
Nikhila Kulkarni <nikhila.kulkarni@revance.com>
2021-09-09 18:06:06 -0700

    Fixing datatypes

--- sql/matillion/ir_commercial/Opul/DDL/ddl_opul_fact_transactions.sql
+++ sql/matillion/ir_commercial/Opul/DDL/ddl_opul_fact_transactions.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS ir_commercial_opul.opul_fact_transactions
   ,device_id VARCHAR(64)  ENCODE raw
   ,created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE raw
   ,updated_at  TIMESTAMP WITHOUT TIME ZONE   ENCODE raw
-  ,staff_id  BIGINT   ENCODE raw
+  ,staff_id  VARCHAR(max)   ENCODE raw
   ,gratuity_amount  NUMERIC(18,2)   ENCODE raw
   ,customer_type VARCHAR(64)   ENCODE raw
   ,payment_detail VARCHAR(max)   ENCODE raw
