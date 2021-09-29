cb763559a3d145d2d878e211f2100a8c13fb6888
Vijay Tummala <vijay.tummala@revance.com>
2021-09-28 20:41:53 -0700

    fix tz

--- sql/matillion/external_reporting/opul/parameterized/sql/fact_deposit_details.sql
+++ sql/matillion/external_reporting/opul/parameterized/sql/fact_deposit_details.sql
@@ -317,9 +317,9 @@ main as
 
 SELECT 
     a.*
-    ,extract (epoch from a.transaction_date) as epoch_transaction_date
-    ,extract (epoch from a.funding_date) as epoch_funding_date
-    ,extract (epoch from a.settled_at_date)  as epoch_settled_at_date
+    ,extract (epoch from CONVERT_TIMEZONE('America/Los_Angeles','UTC',a.transaction_date)) as epoch_transaction_date
+    ,extract (epoch from CONVERT_TIMEZONE('America/Los_Angeles','UTC',a.funding_date)) as epoch_funding_date
+    ,extract (epoch from CONVERT_TIMEZONE('America/Los_Angeles','UTC',a.settled_at_date))  as epoch_settled_at_date
     ,current_timestamp::timestamp as dwh_created_at 
 FROM main a
 JOIN dwh_opul${environment}.dim_practice_odf_mapping b on a.merchant_id = b.card_processing_mid
