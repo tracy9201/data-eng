9fe36e7d3b92f38641a172d33315aa6d3656393e
Joe Shea <joe.shea@revance.com>
2021-09-30 08:08:15 -0400

    updated control table name

---
+++ redshift/admin/data_governance/ddl_encrypt_sensitive_data_ctl_log.sql
@@ -0,0 +1,40 @@
+-- public.encrypt_sensitive_data_ctl_log definition
+
+-- Drop table
+
+--DROP TABLE public.encrypt_sensitive_data_ctl_log;
+
+
+CREATE TABLE IF NOT EXISTS public.encrypt_sensitive_data_ctl_log
+(
+	id INTEGER  DEFAULT "identity"(8548400, 0, '0,1'::text) ENCODE az64
+	,_env_dw VARCHAR(64) NOT NULL DEFAULT 'qa'::character varying ENCODE lzo
+	,_host VARCHAR(1000) NOT NULL DEFAULT 'qa-reporting-cluster.cmmotnszowfl.us-east-1.redshift.amazonaws.com'::CHARACTER varying  ENCODE lzo
+	,_database VARCHAR(64) NOT NULL DEFAULT 'reportingdb'::character varying ENCODE lzo
+	,_schema VARCHAR(127) NOT NULL  ENCODE lzo
+	,_table VARCHAR(127) NOT NULL  ENCODE lzo
+	,_column VARCHAR(127) NOT NULL  ENCODE lzo
+	,sensitive_data_type VARCHAR(1000) not null DEFAULT 'PII'::character varying  ENCODE lzo
+	,is_ready_for_encryption BOOLEAN NOT NULL DEFAULT true ENCODE RAW
+	,is_encrypted BOOLEAN NOT NULL DEFAULT false ENCODE RAW
+	,encryption_start TIMESTAMP WITH TIME ZONE   ENCODE az64
+	,encryption_end TIMESTAMP WITH TIME ZONE   ENCODE az64
+	,encrypted_row_count INTEGER   ENCODE RAW
+	,is_encryption_confirmed BOOLEAN NOT NULL DEFAULT false ENCODE RAW
+    ,code_message VARCHAR(2000) ENCODE lzo
+	,audit_by VARCHAR(1000)   ENCODE lzo
+	,audit_on TIMESTAMP WITH TIME ZONE   ENCODE az64
+	,audit_comment VARCHAR(2000)   ENCODE lzo
+	,inserted_by VARCHAR(100) NOT NULL  ENCODE lzo
+	,inserted_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::character varying)::timestamp with time zone ENCODE az64
+	,inserted_comment VARCHAR(2000)   ENCODE lzo
+	,PRIMARY KEY (_host, _database, _schema, _table, _column)
+)
+DISTSTYLE AUTO
+;
+ALTER TABLE public.encrypt_sensitive_data_ctl_log owner to qadbadmin;
+
+-- Permissions
+
+GRANT INSERT, SELECT, UPDATE, DELETE, RULE, REFERENCES, TRIGGER ON TABLE public.encrypt_sensitive_data_ctl_log TO qadbadmin;
+GRANT SELECT ON TABLE public.encrypt_sensitive_data_ctl_log TO looker;
