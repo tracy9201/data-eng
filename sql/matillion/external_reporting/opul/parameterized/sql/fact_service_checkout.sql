73e6af2d0d5ba8b76d6a995888abdcc671398040
Larry Hui <larry.hui@revance.com>
2021-10-26 16:48:18 +0000

    fact_service_checkout.sql created online with Bitbucket

--- sql/matillion/external_reporting/opul/parameterized/sql/fact_service_checkout.sql
+++ sql/matillion/external_reporting/opul/parameterized/sql/fact_service_checkout.sql
@@ -1,48 +1,32 @@
-WITH 
-  main as
-  (
-    select
-      invoice.id AS invoice_id
-      ,invoice_item.id AS invoice_item_id
-      ,case when invoice.pay_date is null then invoice.updated_at else invoice.pay_date end as pay_date
-      ,round(cast(subscription.quantity as numeric),2)/100 as units
-      ,subscription.unit_name as unit_type
-      ,(invoice_item.subtotal+ invoice_item.discounts)*100/subscription.quantity as price_unit
-      ,round(cast((invoice_item.subtotal + invoice_item.discounts) as numeric),2)/100 as total_price
-      ,round(cast((invoice_item.subtotal) as numeric),2)/100 as discounted_price
-      ,round(cast(invoice_item.discounts as numeric),2)/100 as item_discount
-      ,round(cast(invoice_item.tax as numeric),2)/100 as tax
-      ,discount.note as discount_reason
-      ,round(cast(invoice_item.total as numeric),2)/100 as grand_total
-      ,subscription.encrypted_ref_id as gx_subscription_id
-      ,customer.encrypted_ref_id as gx_customer_id
-      ,provider.encrypted_ref_id as gx_provider_id
-      ,invoice_item.offering_id
-      FROM gaia_opul${environment}.invoice invoice
-      LEFT JOIN gaia_opul${environment}.invoice_item invoice_item on invoice_item.invoice_id = invoice.id
-      LEFT JOIN gaia_opul${environment}.subscription subscription on invoice_item.subscription_id = subscription.id
-      LEFT JOIN gaia_opul${environment}.discount discount on discount.subscription_id = subscription.id
-      LEFT JOIN gaia_opul${environment}.plan plan on subscription.plan_id = plan.id
-      LEFT JOIN gaia_opul${environment}.customer customer on plan.customer_id = customer.id
-      LEFT JOIN gaia_opul${environment}.provider provider on customer.provider_id = provider.id
-    	
-      WHERE invoice.status=20 
-  ) 
-
-  SELECT 
-    invoice_item_id,
-    pay_date,
-    units,
-    unit_type,
-    round(cast(price_unit as numeric)/100,2) as price_unit,
-    total_price,
-    item_discount,
-    discount_reason,
-    discounted_price,
-    grand_total,
-    offering_id,
-    gx_customer_id,
-    gx_provider_id,
+WITH main as (
+select  
+    pci.id as offering_id,
+    pci.name as offering_name, 
+    mci.name as catalog_name, 
+    pci.master_catalog_item_id as catalog_id, 
+    brand.name as factory_name, 
+    mci.brand_id as factory_id, 
+    round(cast(pci.pay_in_full_price as numeric )/100,2) as offering_pay_in_full_price, 
+    round(cast(pci.subscription_price as numeric )/100,2) as offering_subscription_price, 
+    round(cast(pci.tax_rate as numeric )/100,2) as offering_service_tax, 
+    pci.status as offering_status, 
+    pci.organization_id, 
+    pci.created_at as offering_created_at, 
+    mci.created_at as catalog_created_at, 
+    mci.status as catalog_status, 
+    round(cast(mci.wholesale_price as numeric )/100,2) as master_wholesale_price, 
+    mci.sku,
+    round(cast(pci.wholesale_price as numeric )/100,2) as practice_wholesale_price,
+    mci.item_type,
+    cic.name as item_category,
+    brand.created_at as brand_created_at,
+    pci.created_at,
+    pci.updated_at,
     current_timestamp::timestamp as dwh_created_at
-  FROM main
- 
+  FROM catalog${environment}.master_catalog_item mci
+      left join catalog${environment}.brand on mci.brand_id = brand.id
+      LEFT JOIN catalog${environment}.catalog_item_category cic on cic.id = mci.primary_category_id
+      left join catalog${environment}.practice_catalog_item  pci on mci.id = pci.master_catalog_item_id
+  WHERE pci.id is not null
+)
+SELECT * FROM main
