185f5643f754da166886462c41358895cd42da39
Larry Hui <larry.hui@revance.com>
2021-11-04 23:10:33 +0000

    ddl_brand_catalog_category.sql edited online with Bitbucket

--- sql/matillion/ir_cs/DDL/ddl_brand_catalog_category.sql
+++ sql/matillion/ir_cs/DDL/ddl_brand_catalog_category.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS internal_reporting_hint.ddl_brand_catalog_category
   brand VARCHAR(255) ENCODE raw
 ,catalog VARCHAR(255) ENCODE raw
 ,category VARCHAR(255) ENCODE raw
+,dw_created_at timestamp
 )
 ;
