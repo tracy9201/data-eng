df7b56ae6612f1ff5d50ec62bbc7fad357d438ea
Larry Hui <larry.hui@revance.com>
2021-12-13 20:14:51 +0000

    salesforce_extract.sql edited online with Bitbucket

--- sql/matillion/ir_cs/salesforce_extract.sql
+++ sql/matillion/ir_cs/salesforce_extract.sql
@@ -6,7 +6,8 @@ SELECT
   upper(zendesk_support_organization_id_temp__c), 
   revance_territory__C, 
   revance_region__c, 
-  upper(opul_organization_id__c)
-FROM salesforcenew.account 
+  upper(opul_organization_id__c),
+  revance_customer_number__c
+FROM rvsalesforce.account 
 where zendesk_support_organization_id_temp__c is not null 
   or opul_organization_id__c is not null
