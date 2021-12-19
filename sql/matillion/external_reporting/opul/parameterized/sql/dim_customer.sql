fc7697060b250f73f549b6de1498ad34c0daf63f
Larry Hui <larry.hui@revance.com>
2021-12-19 07:26:31 +0000

    dim_customer.sql edited online with Bitbucket fixing guest name issue

--- sql/matillion/external_reporting/opul/parameterized/sql/dim_customer.sql
+++ sql/matillion/external_reporting/opul/parameterized/sql/dim_customer.sql
@@ -1,39 +1,4 @@
-with guest_name as (
-SELECT distinct
-    cud.gx_customer_id,
-    card_holder_name
-FROM
-    gaia_opul${environment}.payment p
-LEFT JOIN
- (SELECT distinct 
-  gt.payment_id, 
-  card_payment_gateway_id
-FROM  gaia_opul${environment}.gateway_transaction gt
-WHERE 
-  card_payment_gateway_id IS NOT NULL and card_payment_gateway_id != 0
-  and gt.payment_id is not null) gt
-        ON gt.payment_id = p.id
-LEFT JOIN
-    gaia_opul${environment}.card_payment_gateway cpg
-        ON cpg.id = gt.card_payment_gateway_id
-LEFT JOIN
-    gaia_opul${environment}.card card
-        ON cpg.card_id = card.id
-left join 
-    gaia_opul${environment}.plan plan
-        on p.plan_id = plan.id
-left join 
-    gaia_opul${environment}.customer cus
-        on cus.id = plan.customer_id
-left join 
-    kronos_opul${environment}.customer_data cud
-        on cus.encrypted_ref_id = cud.gx_customer_id
-WHERE
-    p.id IS NOT NULL
-    AND p.status  in (1,-3)
-    AND p.type = 'credit_card'
-    AND cud.type = 1
- ),
+with 
  
 sub_plan AS 
 (SELECT
@@ -68,7 +33,6 @@ customer AS
     customer_data.type AS user_type,
     firstname,
     lastname,
-    guest_name.card_holder_name as full_name,
     users.created_at,
     users.updated_at,
     current_timestamp::timestamp as dwh_created_at
@@ -77,7 +41,6 @@ JOIN kronos_opul${environment}.customer_data customer_data ON users.id = custome
 LEFT JOIN kronos_opul${environment}.plan plan ON plan.user_id = users.id  
 LEFT JOIN sub_plan ON sub_plan.plan_id = plan.id  
 LEFT JOIN kronos_opul${environment}.address address ON billing_address_id = address.id 
-LEFT JOIN guest_name on customer_data.gx_customer_id = guest_name.gx_customer_id
 ) ,
 
 main as
@@ -101,7 +64,6 @@ main as
                   END,' ') AS customer_type,
     firstname,
     lastname,
-    case when user_type = 0 then firstname||' '||lastname else full_name end as full_name,
     created_at,
     updated_at,
     dwh_created_at  
