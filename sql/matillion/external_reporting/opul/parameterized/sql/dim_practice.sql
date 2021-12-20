7ed67f02dd5e389aa00071d7fb726071e19b6913
Larry Hui <larry.hui@revance.com>
2021-12-20 09:59:13 +0000

    dim_practice.sql edited online with Bitbucket for https://hintmd.atlassian.net/browse/OPLA-964

--- sql/matillion/external_reporting/opul/parameterized/sql/dim_practice.sql
+++ sql/matillion/external_reporting/opul/parameterized/sql/dim_practice.sql
@@ -8,7 +8,9 @@ WITH main as
     city AS practice_city,
     state AS practice_state,
     zip AS practice_zip,
-    merchant_id,
+    organization_data.merchant_id,
+    merchant.type_of_practice,
+    contact.email,
     organization_data.created_at,
     organization_data.updated_at,
     current_timestamp::timestamp as dwh_created_at
@@ -20,7 +22,15 @@ JOIN
 JOIN
     kronos_opul${environment}.address  address        
         ON address.id = address_id  
+JOIN 
+    merchant${environment}.merchant merchant 
+        on organization_data.merchant_id = merchant.id
+JOIN 
+    merchant${environment}.contact contact 
+        on contact.merchant_id = organization_data.merchant_id
+	
 WHERE
     role = 7
+    and contact.contact_type = 'DBA'
 )
 SELECT * FROM main
